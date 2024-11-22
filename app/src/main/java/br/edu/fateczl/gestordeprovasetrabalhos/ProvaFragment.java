46a7c461b84bbce09301706f659c40ea283b5f22
Matheus Marti <30555147+Matheus-Marti1@users.noreply.github.com>
2024-11-22 20:38:50 -0300

    Fazer ligações iniciais no ProvaFragment

--- app/src/main/java/br/edu/fateczl/gestordeprovasetrabalhos/ProvaFragment.java
+++ app/src/main/java/br/edu/fateczl/gestordeprovasetrabalhos/ProvaFragment.java
@@ -7,9 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ProvaFragment extends Fragment {
     private View view;
+    private EditText etIdProva, etDescricaoProva, etDataProva, etPesoProva, etNotaProva;
+    private CheckBox cbRecuperacaoProva;
+    private Spinner spDisciplinaProva;
+    private Button btnBuscarProva, btnInserirProva, btnModificarProva, btnExcluirProva, btnListarProva;
+    private TextView tvListaProva;
 
     public ProvaFragment() {
         super();
@@ -19,6 +29,19 @@ public class ProvaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_prova, container, false);
+        etIdProva = view.findViewById(R.id.etIdProva);
+        etDescricaoProva = view.findViewById(R.id.etDescricaoProva);
+        etDataProva = view.findViewById(R.id.etDataProva);
+        etPesoProva = view.findViewById(R.id.etPesoProva);
+        etNotaProva = view.findViewById(R.id.etNotaProva);
+        cbRecuperacaoProva = view.findViewById(R.id.cbRecuperacaoProva);
+        spDisciplinaProva = view.findViewById(R.id.spDisciplinaProva);
+        btnBuscarProva = view.findViewById(R.id.btnBuscarProva);
+        btnInserirProva = view.findViewById(R.id.btnInserirProva);
+        btnModificarProva = view.findViewById(R.id.btnModificarProva);
+        btnExcluirProva = view.findViewById(R.id.btnExcluirProva);
+        btnListarProva = view.findViewById(R.id.btnListarProva);
+        tvListaProva = view.findViewById(R.id.tvListaProva);
         return view;
     }
 }
